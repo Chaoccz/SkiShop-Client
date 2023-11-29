196c7bc303663151b4bdb2a72aebc32dbc043cc1
Chao Gao <chaogao686@gmail.com>
2023-11-29 05:39:34 -0600

    Identity

--- client/src/app/core/nav-bar/nav-bar.component.ts
+++ client/src/app/core/nav-bar/nav-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AccountService } from 'src/app/account/account.service';
+
 import { BasketService } from 'src/app/basket/basket.service';
 import { BasketItem } from 'src/app/shared/models/basket';
 
@@ -8,7 +10,10 @@ import { BasketItem } from 'src/app/shared/models/basket';
   styleUrls: ['./nav-bar.component.scss'],
 })
 export class NavBarComponent {
-  constructor(public basketService: BasketService) {}
+  constructor(
+    public basketService: BasketService,
+    public accountService: AccountService
+  ) {}
 
   getCount(items: BasketItem[]) {
     return items.reduce((sum, item) => sum + item.quantity, 0);
