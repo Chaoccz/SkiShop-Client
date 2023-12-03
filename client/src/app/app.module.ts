c3d0456be5d482cb260e55536fa95cd04da87176
Chao Gao <chaogao686@gmail.com>
2023-12-03 11:10:48 -0600

    Checkout

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -9,6 +9,7 @@ import { CoreModule } from './core/core.module';
 import { HomeModule } from './home/home.module';
 import { ErrorInterceptor } from './core/interceptors/error.interceptor';
 import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
+import { JwtInterceptor } from './core/interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [AppComponent],
@@ -29,6 +30,11 @@ import { LoadingInterceptor } from './core/interceptors/loading.interceptor';
     {
       provide: HTTP_INTERCEPTORS,
       useClass: LoadingInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: JwtInterceptor,
       multi: true,
     },
   ],
